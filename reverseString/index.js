2925cdda59d09021e2dff4c7f5a2fae9ce7aef05
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-10-16 16:34:27 -0400

    reversedstring

--- reverseString/index.js
+++ reverseString/index.js
@@ -1,1 +1,10 @@
 console.log("Reverse String Solution")
+
+const reverseString = (word) => {
+    const wordArray = word.split('');
+    const reversedWordArray = wordArray.reverse();
+    const reversedWord = reversedWordArray.join('');
+    return reversedWord
+}
+
+console.log("Reversed word is: ", reverseString("hello there how are you?"))
