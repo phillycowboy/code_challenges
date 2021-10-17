f133080ec3a94d35f2639bc54552d8af4cea8790
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-10-16 21:21:10 -0400

    notes

--- findTheVowel/index.js
+++ findTheVowel/index.js
@@ -21,6 +21,8 @@ const findTheVowels = (string) => {
     return count;
 }
 
+// Point to be noted that we use “includes()” method which is applicable both in string and array. Its return true or false base of the character.
+
 console.log("should return 5!", findTheVowels('aeiou'))
 console.log("should return 2!", findTheVowels('letter'))
 console.log("should return 0!", findTheVowels('crypt'))
