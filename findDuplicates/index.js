cd287e5c9227165697d86db29e7545edba6a7d88
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-10-18 16:45:21 -0400

    solution with filter instead of Set

--- findDuplicates/index.js
+++ findDuplicates/index.js
@@ -7,3 +7,12 @@ const findDups = (array) => {
 
 console.log("array is [1,2,3,3,3,3,3,4,5,6,7,7,7,8,9,10]", findDups([1,2,3,3,3,3,3,4,5,6,7,7,7,8,9,10]))
 console.log("array is [7,7,7,7,7,7,7,7,7,7,7]", findDups([7,7,7,7,7,7,7,7,7,7,7]))
+
+const findDups2 = (array) => {
+    return array.filter((item, pos, arr) => {
+        return !pos || item != arr[pos - 1]
+    })
+}
+
+console.log("array is [1,2,3,3,3,3,3,4,5,6,7,7,7,8,9,10]", findDups2([1,2,3,3,3,3,3,4,5,6,7,7,7,8,9,10]))
+console.log("array is [7,7,7,7,7,7,7,7,7,7,7]", findDups2([7,7,7,7,7,7,7,7,7,7,7]))
