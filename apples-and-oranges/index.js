aa6741548fa43360210304c4dc9da743b6d8d4ca
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-11-02 10:08:55 -0400

    added notes

--- apples-and-oranges/index.js
+++ apples-and-oranges/index.js
@@ -1,1 +1,12 @@
 console.log("Apples and Oranges Solution");
+// Sams house has an apple tree and an orange tree that yield an abundance of fruit. 
+// using the information below determine the number of apples and oranges that land on sams house 
+
+// the red region denotes the house, and s is the start point and t is the endpoint. the apple tree is to the left of the house and the orange tree is to the right
+// assume the tree are located on a single point,where the apple tree is at point a and the orange tree is at point b
+
+// when a fruit falls from a tree it lands d units of distance along the x axis a negative value of d mean that the fruit fell d units to the trees left and a positive 
+// value of d means that the fruit fell d units to the right. 
+
+// given the d value of m apples and n oranges determine how many apples and oranges will fall on sams house(i.e the inclusive range of [s,t])
+
