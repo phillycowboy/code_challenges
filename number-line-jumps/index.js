f1d9b7cc7e96bef125dc13fc7a5518a6d435b703
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-11-02 13:21:30 -0400

    finished solution

--- number-line-jumps/index.js
+++ number-line-jumps/index.js
@@ -9,3 +9,16 @@ console.log("Number Line Jumps");
 
 // you have to figure out a way to get both kangaroos at the same location at the same time as part of the show if it is possible return YES otherwise return NO. 
 
+function kangaroo(x1, v1, x2, v2){
+    let result = "NO";
+    for(let i = 0; i < 10000 && result === "NO"; i++){
+        if(x1 + v1 * i === x2 + v2 * i){
+            // checking to see if the kangaroos are in the same spot.
+            result = "YES"
+        }
+    }
+    return result;
+}
+
+console.log(kangaroo(0,3,4,2));
+console.log(kangaroo(0,2,3,4));
