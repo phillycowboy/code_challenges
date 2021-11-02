069ffa197eae7c19b841c3579b5b2cd9d595be61
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-11-02 12:58:11 -0400

    added files and notes

---
+++ number-line-jumps/index.js
@@ -0,0 +1,11 @@
+console.log("Number Line Jumps");
+
+// you are coreographing a circus show with various animals. for one act you are given two kangaroos on a number line ready to jump in the positive direction 
+// (i.e. towards positive infinity)
+
+// the first kangaroo starts at location x1 and moves at a rate of v1 meters per jump 
+
+// the second kangaroo starts at location x2 and moves at a rate of v2 meters per jump 
+
+// you have to figure out a way to get both kangaroos at the same location at the same time as part of the show if it is possible return YES otherwise return NO. 
+
